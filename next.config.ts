84af607def76709cea6076c55e37265c00e7c343
chinhvslenzoloo <chinhvslenzoloo@gmail.com>
2025-04-11 18:45:49 +0800

    3

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // <- энэ хэсгийг нэмсэн
+  },
+  // бусад config бол энд...
 };
 
 export default nextConfig;
